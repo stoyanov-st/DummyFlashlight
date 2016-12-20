2676fc01824d56b99a0a9986b0e076f1ab9af783
stoyanov-st <st.stoyanov04@gmail.com>
2016-12-20 23:12:23 +0200

    Design Changes

--- app/src/main/java/com/redflagsoft/stoyanovst/mydummyflashlight/MainActivity.java
+++ app/src/main/java/com/redflagsoft/stoyanovst/mydummyflashlight/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,15 +17,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
+import android.os.Handler;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Button toggleSwitch;
+    private Camera cam;
+    private Camera.Parameters p;
+    private Handler handler;
+    private Runnable runnable;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Simple Dummy Flashlight by Stoyanov's RedFlagSoft", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        toggleSwitch = (Button) findViewById(R.id.button);
+        toggleSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler = new Handler(Looper.getMainLooper());
+                runnable = new Runnable(){
+                    @Override
+                    public void run(){
+                        letThereBeLight();
+                    }
+                };
+                handler.post(runnable);
             }
         });
     }
@@ -50,57 +75,68 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            forDummies();
+            handler = new Handler(Looper.getMainLooper());
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    forDummies();
+                }
+            };
+            handler.post(runnable);
+
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    Camera cam  = Camera.open();
-    Camera.Parameters p = cam.getParameters();
+    boolean isItTurned = false;
 
-    public void testVersion(View view){
-
-        Button toggleSwitch = (Button) findViewById(R.id.button);
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
-            letThereBeLight(cam, toggleSwitch, p);
+    public void changeLabel(){
+        if(isItTurned){
+            toggleSwitch.setText(R.string.button_text_off);
         }
-        else    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            try {
-                lightForNewAndroid(toggleSwitch);
-            } catch (CameraAccessException e) {
-                e.printStackTrace();
-            }
-        }
+        else toggleSwitch.setText(R.string.button_text_on);
     }
 
-    public void letThereBeLight(Camera cam, Button toggleSwitch, Camera.Parameters p) {
+    @SuppressWarnings("deprecation")
+    public void letThereBeLight() {
 
-                if (toggleSwitch.getText() == getString(R.string.button_text_on)) {
-                    try {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+
+                    if (!isItTurned) {
+                        cam = Camera.open();
                         p = cam.getParameters();
                         p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
                         cam.setParameters(p);
                         cam.startPreview();
-                        toggleSwitch.setText(R.string.button_text_off);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                        Toast.makeText(getBaseContext(), "Exception turning on", Toast.LENGTH_SHORT).show();
-                    }
-                } else if (toggleSwitch.getText() == getString(R.string.button_text_off)){
-                    try {
-                        toggleSwitch.setText(R.string.button_text_on);
+                        isItTurned = !isItTurned;
+                    } else {
+                        cam.reconnect();
                         p = cam.getParameters();
                         p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
                         cam.setParameters(p);
                         cam.stopPreview();
-
-                    } catch (Exception e){
-                        e.printStackTrace();
-                        Toast.makeText(getBaseContext(), "Exception turning off", Toast.LENGTH_SHORT).show();
+                        cam.release();
+                        isItTurned = !isItTurned;
                     }
                 }
+
+                catch(IOException e) {
+                    cam.release();
+                    cam = Camera.open();
+                }
+                finally {
+                    changeLabel();
+                }
+            }
+
+        };
+        handler.post(runnable);
+
     }
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -120,47 +156,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void forDummies(){
-        Toast.makeText(MainActivity.this, "Access Denied!", Toast.LENGTH_LONG).show();
+        handler = new Handler(Looper.getMainLooper());
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "Access Denied!", Toast.LENGTH_LONG).show();
+            }
+        };
+        handler.post(runnable);
     }
-
-    @Override
-    protected void onDestroy() {
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
-            if (cam != null) {
-                cam.release();
-            }
-            if (p != null){
-                p = null;
-            }
-        }
-            super.onDestroy();
-
-    }
-
-    @Override
-    protected void onPause(){
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
-            if (cam != null) {
-                cam.release();
-            }
-            if (p != null){
-               p = null;
-            }
-        }
-            super.onPause();
-    }
-
-    @Override
-    protected void onResume(){
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP){
-            if (p == null){
-                cam = Camera.open();
-                p = cam.getParameters();
-                Button toggleSwitch = (Button) findViewById(R.id.button);
-                toggleSwitch.setText(R.string.button_text_on);
-            }
-        }
-        super.onResume();
-    }
-
 }
